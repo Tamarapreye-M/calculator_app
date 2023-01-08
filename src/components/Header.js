23615c0cd71345916310f2e0c642e8b54f80fec1
Tamarapreye-M <preyemichael116@gmail.com>
2023-01-08 21:05:50 +0100

    Heaader structured

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,16 @@ const Header = () => {
 	return (
 		<header>
 			<h1>calc</h1>
-			<div>
+			<div className="themes">
 				<p>THEME</p>
-				<div>
+				<div className="theme-lists">
 					<div>
 						<span>1</span>
 						<span>2</span>
 						<span>3</span>
 					</div>
-					<div>
-						<div></div>
+					<div className="control-box">
+						<div className="control-ball"></div>
 					</div>
 				</div>
 			</div>
