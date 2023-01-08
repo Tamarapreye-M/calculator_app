bacad43d9e6e0ea6c17596cea57295276bc53e93
Tamarapreye-M <preyemichael116@gmail.com>
2023-01-08 21:58:39 +0100

    fixed state management bugs

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,28 +1,17 @@
 import { type } from "@testing-library/user-event/dist/type";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const Button = (prop) => {
 	const { setCalculated, calculated, name, content } = prop;
-
-	const initialVals = {
-		numbers: "0",
-		plus: "",
-		divide: "",
-		minus: "",
-		times: "",
-		del: "",
-		previous: "",
-		current: "",
-	};
 	const handleDisplay = (ev) => {
 		let { value } = ev.target;
 
-		// setCalculated((prev) => (!isNaN(+value) ? +(prev + value) : prev + value));
+		value.toLowerCase() === "x".trim() && (value = " * ");
+
 		value !== "del" && value !== "=" && setCalculated((prev) => prev + value);
 
 		value === "reset" && setCalculated((prev) => "");
 		value === "del" && setCalculated((prev) => `${prev}`.slice(0, -1));
-		value.toLowerCase() === "x".trim() && (value = " * ");
 
 		value === "=" &&
 			setCalculated((prev) => {
@@ -32,20 +21,10 @@ const Button = (prop) => {
 					.join("");
 				return eval(cleanUp);
 			});
-		// console.log(calculated);
-
-		// calculated[0] ==='0' && is
-
-		// setCalculated((prev) => prev + value);
-
-		// console.log(value);
 	};
-	let num = ["01", "0.01", "002", "+", "-"];
-	console.log(num.map((each) => (isNaN(+each) ? each : `${+each}`)).join(""));
-	// console.log(Number.isInteger(num));
 	return (
 		<button className={name} value={content} onClick={handleDisplay}>
-			{prop.content}
+			{content}
 		</button>
 	);
 };
