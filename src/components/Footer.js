fea7c3cf1726aaae1da9e7840bb9252ac29cab76
Tamarapreye-M <preyemichael116@gmail.com>
2023-01-09 12:55:24 +0100

    fixing bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,8 @@ const Footer = () => {
 	return (
 		<footer className="attribution">
 			Challenge by
-			<a
-				href="https://www.frontendmentor.io?ref=challenge"
-				target={"_blank"}
-				rel="noreferrer"
-			>
-				Frontend Mentor
-			</a>
-			. Coded by <a href="#">Tamarapreye Michael</a>.
+			<button>Frontend Mentor</button>. Coded by{" "}
+			<button>Tamarapreye Michael</button>.
 		</footer>
 	);
 };
